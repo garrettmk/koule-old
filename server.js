3698b01e3125cf5de183f6d131e12e9ebf73aa0f
garrett <garrettmyrick@gmail.com>
2019-09-17 23:13:26 -0700

    Updated server.js.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + '/build'));
+app.use('/*', express.static(__dirname + '/build'));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
